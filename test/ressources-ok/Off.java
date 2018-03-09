eb8fb17d2469a50dff22172d23602cd2dc091a2c
loicbourgois <loic@bourgois.pro>
2018-03-09 07:13:07 +0100

    Add On/Off to checkstyle

---
+++ test/ressources-ok/Off.java
@@ -0,0 +1,8 @@
+//CHECKSTYLE.OFF: OuterTypeFilenameCheck
+//CHECKSTYLE.OFF: IndentationCheck
+public class HelloWorld {
+public static void main(String[] args) {
+// Prints "Hello, World" to the terminal window.
+System.out.println("Hello, World");
+}
+}
